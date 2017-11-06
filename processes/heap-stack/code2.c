21a85e3a3f30a40561d828def8546c3fd155066d
GaPhil <gaphil.io@gmail.com>
2017-11-06 13:05:53 +0100

    Add processes

---
+++ processes/heap-stack/code2.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+char global[] = "This is a global sring";
+const int read_only = 123456;
+
+int main() {
+	int pid = getpid();
+foo:
+	printf("proccess id: %d\n", pid);
+	printf("global string: %p\n", &global);
+	printf("the code: %p\n", &&foo);
+	printf("read only: %p\n", &read_only);
+
+	printf("\n\n /proc/%d/maps \n\n", pid);
+	char command[50];
+	sprintf(command, "cat /proc/%d/maps", pid);
+	system(command);
+
+	return 0;
+}
