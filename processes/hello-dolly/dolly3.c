c40663f41ccca9197a3291812981f9887b66d828
GaPhil <gaphil.io@gmail.com>
2017-11-06 14:12:56 +0100

    Add hello-dolly

---
+++ processes/hello-dolly/dolly3.c
@@ -0,0 +1,22 @@
+// wait a minute
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+int main() {
+
+	int pid = fork();
+
+	if (pid == 0) {
+		printf("I am the child: %d\n", getpid());
+		sleep(1);
+	} else {
+		printf("My child is called %d\n", pid);
+		wait(NULL);
+		printf("My child has terminated \n");
+	}
+	printf("This is the end (%d)\n", getpid());
+
+	return 0;
+}
