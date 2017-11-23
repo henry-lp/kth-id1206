373cc58394f454912464f1b9be41a366bf8238f3
GaPhil <gaphil.io@gmail.com>
2017-11-23 10:28:29 +0100

    Update long_mode

--- project/minimal-x86-kernel/src/arch/x86_64/long_mode_init.asm
+++ project/minimal-x86-kernel/src/arch/x86_64/long_mode_init.asm
@@ -11,6 +11,10 @@ long_mode_start:
 	mov fs, ax
 	mov gs, ax
 
+	; call the rust main
+    extern rust_main
+    call rust_main
+
 	; print 'OKAY' to screen
 	mov rax, 0x2f4c2f492f482f50
 	mov qword [0xb8000], rax
