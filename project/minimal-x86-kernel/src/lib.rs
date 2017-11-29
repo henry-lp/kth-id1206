db47c3da0da26329ebadc4f56c9eea1745e54393
GaPhil <gaphil.io@gmail.com>
2017-11-29 19:13:01 +0100

    Rust stuff

--- project/minimal-x86-kernel/src/lib.rs
+++ project/minimal-x86-kernel/src/lib.rs
@@ -6,3 +6,11 @@ pub extern fn rust_main() {}
 
 #[lang = "eh_personality"] extern fn eh_personality() {}
 #[lang = "panic_fmt"] #[no_mangle] pub extern fn panic_fmt() -> ! {loop{}}
+
+extern crate rlibc;
+
+#[no_mangle]
+pub extern fn rust_main() {
+    let x = ["Hello", "World", "!"];
+    let y = x;
+}
