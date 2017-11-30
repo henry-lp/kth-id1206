d67fa2180bba80ae8d389af3a277bb519eea9269
GaPhil <gaphil.io@gmail.com>
2017-11-30 09:08:56 +0100

    Fix lib.rs

--- project/minimal-x86-kernel/src/lib.rs
+++ project/minimal-x86-kernel/src/lib.rs
@@ -1,6 +1,7 @@
 #![feature(lang_items)]
 #![no_std]
 #![feature(unique)]
+#![feature(const_unique_new)]
 
 #[lang = "eh_personality"] extern fn eh_personality() {}
 #[lang = "panic_fmt"] #[no_mangle] pub extern fn panic_fmt() -> ! {loop{}}
