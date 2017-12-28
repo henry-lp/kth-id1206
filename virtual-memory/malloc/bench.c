99aa05700aee7523ec00607f3f84b45b9a3808ac
GaPhil <gaphil.io@gmail.com>
2017-12-28 15:51:58 +0100

    Add random to bench.c

--- virtual-memory/malloc/bench.c
+++ virtual-memory/malloc/bench.c
@@ -5,39 +5,10 @@
 
 #define ROUNDS 100
 #define LOOP 100000
+#define BUFFER 100
+
 #define MAX 4000
 #define MIN 8
-
-int main() {
-
-	void *init = sbrk(0);	// get current top of the heap
-	void *current;
-
-	printf("The initial top of the heap is %p.\n", init);
-
-	for(int j = 0; j < ROUNDS; j++) {
-		for(int i = 0; i < LOOP; i++) {
-			size_t size = (rand() % 4000) + sizeof(int);
-			int *memory;
-			memory = malloc(size);
-			if(memory == NULL) {
-				fprintf(stderr, "malloc failed\n");
-				return(1);
-			}
-			/* writing to the memory so we know it exists */
-//			*memory = 123;
-			free(memory);
-		}
-		current = sbrk(0);
-		int allocated = (int)((current - init) / 1024);
-		printf("%d\n", j);
-		printf("The current top of the heap is %p.\n", current);
-		printf("	increased by %d Kbyte\n", allocated);
-	}
-
-	return 0;
-}
-
 
 int request() {
 
@@ -52,3 +23,50 @@ int request() {
 
 	return size;
 }
+
+
+int main() {
+
+	void *buffer[BUFFER];
+	for(int i = 0; i < BUFFER; i++) {
+		buffer[i] = NULL;
+	}
+
+	void *init = sbrk(0);	// get current top of the heap
+	void *current;
+
+	printf("The initial top of the heap is %p.\n", init);
+
+	for(int j = 0; j < ROUNDS; j++) {
+		for(int i = 0; i < LOOP; i++) {
+
+			int index = rand() % BUFFER;
+			if(buffer[index] != NULL) {
+				free(buffer[index]);
+			}
+
+			size_t size = (size_t) request();
+			int *memory;
+			memory = malloc(size);
+
+			if(memory == NULL) {
+				fprintf(stderr, "memory allocation failed\n");
+				return(1);
+			}
+
+			buffer[index] = memory;
+			/* writing to the memory so we know it exits */
+			*memory = 123;
+
+			free(memory);
+		}
+		current = sbrk(0);
+		int allocated = (int)((current - init) / 1024);
+		printf("%d\n", j);
+		printf("The current top of the heap is %p.\n", current);
+		printf("	increased by %d Kbyte\n", allocated);
+	}
+
+	return 0;
+}
+
