c445d8f4a01ff361fb71de8930044a9e44247a61
GaPhil <gaphil.io@gmail.com>
2018-01-01 22:06:56 +0100

    Update random.c

--- virtual-memory/page-frame-reclaiming-alg/random.c
+++ virtual-memory/page-frame-reclaiming-alg/random.c
@@ -8,6 +8,18 @@
 /* 20% of the pages will have 80% of the references */
 #define HIGH 20
 #define FREQ 80
+#define PAGES 100
+#define SAMPLES 20
+
+typedef struct pte {
+	int present;
+} pte;
+
+void clear_page_table(pte *page_table, int pages) {
+	for(int i = 0; i < pages; i++) {
+		page_table[i].present = 0;
+	}
+}
 
 void init(int *sequence, int refs, int pages) {
 
@@ -24,21 +36,75 @@ void init(int *sequence, int refs, int pages) {
 	}
 }
 
+int simulate(int *seq, pte *table, int refs, int frms, int pgs) {
+
+	int hits = 0;
+	int allocated = 0;
+
+	int i;
+
+	for(i = 0; i < refs; i++) {
+		int next = seq[i];
+		pte *entry = &table[next];
+
+		if(entry -> present == 1) {
+			hits++;
+		} else {
+			if(allocated < frms) {
+				allocated++;
+				entry -> present = 1;
+			} else {
+				pte *evict;
+				do {
+					int rnd = rand() % pgs;
+					evict = &table[rnd];
+				} while(evict -> present != 1);
+
+				evict -> present = 0;
+				evict -> present = 1;
+			}
+		}
+	}
+	return hits;
+}
+
 int main(int argc, char *argv[]) {
 
+	pte *table = (pte *) malloc(PAGES *sizeof(pte));
+
 	/* could be command line arguments */
-	int refs = 10;
+	int refs = 100000;
 	int pages = 100;
 
 	int *sequence = (int*) malloc (refs *sizeof(int));
 
 	init(sequence, refs, pages);
 
-	/* a small experiment to show that it works */
-	for(int i = 1; i < refs; i++) {
-		printf(", %d", sequence[i]);
+
+	/* compute a increment, the number of pages divided by the number
+	of data pints, and then run the simulation for a growing number
+	of frames. */
+
+	printf("# This is a benchmark of random replacement\n");
+	printf("# %d page references\n", refs);
+	printf("# %d pages \n", pages);
+	printf("#\n# frames\tratio\n");
+
+	/* frames is the size of the memory in frames */
+	int frames;
+
+	int incr = pages / SAMPLES;
+
+	for(frames = incr; frames <= pages; frames += incr) {
+		/* clear page tables entries */
+		clear_page_table(table, pages);
+
+		int hits = simulate(sequence, table, refs, frames, pages);
+
+		float ratio = (float) hits / refs;
+
+		printf("%d\t%.2f\n", frames, ratio);
 	}
-	printf("\n");
 
 	return 0;
 }
