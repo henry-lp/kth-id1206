93802df845a57c0a54763bf5373130907cf02114
GaPhil <gaphil.io@gmail.com>
2018-01-09 20:58:47 +0100

    Initial commit on piping

---
+++ concurrency/ping-pong-and-echoes/cave.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <assert.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+#define ITERATIONS 10
+#define BURST 1000
+
+// producer and process that will create the pipe.
+int main() {
+	/* create the named pipe */
+	int mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH;
+
+	mkfifo("sesame", mode);
+
+	int flag = O_WRONLY;
+	int pipe = open("sesame", flag);
+
+	for(int i = 0; i < ITERATIONS; i++) {
+		for(int j = 0; j < BURST; j++) {
+			//write(descr[1], "0123456789", 10);
+		}
+		printf("producer burst %d done\n", i);
+	}
+
+	printf("producer done\n");
+	return 0;
+}
